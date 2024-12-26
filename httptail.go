6642a4a3f1100a214bf5b215f93d3e2ae09ef53b
StabbyCutyou <smattkelly@gmail.com>
2024-12-25 22:26:05 -0500

    collect more metadata about each request

--- httptail.go
+++ httptail.go
@@ -3,11 +3,13 @@ package httptail
 import (
 	"io"
 	"net/http"
+	"net/http/httptrace"
 )
 
 // HttpRedirect holds metadata about an individual redirect
 type HttpRedirect struct {
 	URL          string      `json:"url"`
+	IP           string      `json:"ip"`
 	RedirectType string      `json:"redirect_type"`
 	RedirectTo   string      `string:"redirect_to"`
 	Headers      http.Header `json:"headers"`
@@ -16,11 +18,9 @@ type HttpRedirect struct {
 
 // HttpRedirectLog holds structured results for each hop in the redirect chain
 type HttpRedirectLog struct {
-	OriginalURL   string         `json:"original_url"`
-	FinalURL      string         `json:"final_url"`
-	FinalHeaders  http.Header    `json:"final_headers"`
-	FinalTrailers http.Header    `json:"final_trailers"`
-	Redirects     []HttpRedirect `json:"redirects"`
+	OriginalURL string         `json:"original_url"`
+	FinalURL    string         `json:"final_url"`
+	Redirects   []HttpRedirect `json:"redirects"`
 }
 
 func Tail(url string) (*HttpRedirectLog, error) {
@@ -30,18 +30,19 @@ func Tail(url string) (*HttpRedirectLog, error) {
 	}
 	c := &http.Client{
 		CheckRedirect: func(req *http.Request, via []*http.Request) error {
-			hr := HttpRedirect{
-				URL:        via[len(via)-1].URL.String(),
-				RedirectTo: req.URL.String(),
-				// TODO - i was trying to get the status from the last request in
-				// via, but the response body was gone by this time
-				// is the requests response status the one we want, or the one in via?
-				// a problem for another day, this is to just prove the approach
-				RedirectType: req.Response.Status,
-				Headers:      req.Response.Header,
-				Trailers:     req.Response.Trailer,
-			}
-			l.Redirects = append(l.Redirects, hr)
+			// By now, the trace resolver has already set up the redirect
+			// etnry we need with the IP, so this captures the rest of that data
+			hr := l.Redirects[len(l.Redirects)-1]
+			hr.URL = via[len(via)-1].URL.String()
+			hr.RedirectTo = req.URL.String()
+			// TODO - i was trying to get the status from the last request in
+			// via, but the response body was gone by this time
+			// is the requests response status the one we want, or the one in via?
+			// a problem for another day, this is to just prove the approach
+			hr.RedirectType = req.Response.Status
+			hr.Headers = req.Response.Header
+			hr.Trailers = req.Response.Trailer
+			l.Redirects[len(l.Redirects)-1] = hr
 			return nil
 		},
 	}
@@ -50,13 +51,30 @@ func Tail(url string) (*HttpRedirectLog, error) {
 	if err != nil {
 		return nil, err
 	}
+	t := &httptrace.ClientTrace{
+		GotConn: func(connInfo httptrace.GotConnInfo) {
+			// TODO what else can we record here?
+			hr := HttpRedirect{
+				IP: connInfo.Conn.RemoteAddr().String(),
+			}
+			l.Redirects = append(l.Redirects, hr)
+		},
+	}
+	r = r.WithContext(httptrace.WithClientTrace(r.Context(), t))
 	resp, err := c.Do(r)
 	if err != nil {
 		return nil, err
 	}
 	// proper hygiene
 	defer resp.Body.Close()
-	l.FinalURL = resp.Request.URL.String()
+	// Record some final metadata about the end request
+	f := l.Redirects[len(l.Redirects)-1]
+	f.URL = resp.Request.URL.String()
+	f.Headers = resp.Header
+	f.Trailers = resp.Trailer
+	l.Redirects[len(l.Redirects)-1] = f
+	// Very easy top level reporting
+	l.FinalURL = f.URL
 	io.Copy(io.Discard, resp.Body)
 	return l, nil
 }
